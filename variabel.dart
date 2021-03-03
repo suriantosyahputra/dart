55c9b49a48455a9c5904c57cd6e60f261a0806ed
suriantosyahputra <78522974+suriantosyahputra@users.noreply.github.com>
2021-03-03 11:54:52 +0700

    Add files via upload

---
+++ variabel.dart
@@ -0,0 +1,15 @@
+void main(){
+    // membaut variabel dengan tipe data
+    String nama = "Syahnita Rizky Utami";
+    int umur = 20;
+    double tinggi = 141;
+    bool isMenikah = false;
+
+    // membuat veriabel dengan kata kunci var
+    var alamat = "Medan, Indonesia";
+
+    // mencetak variabel
+    print("Nama saya $nama. Umur $umur tahun. Tinggi sekitar $tinggi cm.");
+    print("Menikah: $isMenikah");
+    print("Alamat: $alamat");
+}
